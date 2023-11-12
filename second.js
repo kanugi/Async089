bdc22ff77682ed339f0b8eb43d47bb3bd6ce825e
Kanikayudha Tantyo N <kanikayudha.17@students.amikom.ac.id>
2023-11-12 19:11:19 +0700

    change show apikey response by promise

--- second.js
+++ second.js
@@ -1,19 +1,24 @@
 const token = ~~[Math.random() * 123456789];
 const pictures = ["1.jpg", "2.jpg", "3.jpg"];
 
-function login(username, callback) {
+function login(username) {
   console.log("Memvalidasi data...")
-  setTimeout(() => {
-    callback ({username, token})
-  }, 200)
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if(username != "Muhammad Baddar") reject("Username salah")
+      resolve({token: token})
+    }, 200)
+  })
 }
 
-function getUser(token, callback){
+function getUser(token){
   console.log("processing token...")
-  if(token) 
-  setTimeout(() => {
-    callback ( {apiKey: "BJUwh1" })
-  }, 500); 
+  return new Promise((resolve, reject) => {
+    if (!token) reject("token tidak ada")
+    setTimeout(() => {
+      resolve({apiKey: "UBey51"})
+    }, 500)
+  })
 }
 
 function getPictures(apiKey, callback){
@@ -23,28 +28,12 @@ function getPictures(apiKey, callback){
   }, 1500);
 }
 
-login("Muhammad Baddar", function(response){
+const user = login("Muhammad Baddar")
+user.then(function(response) {
   const {token} = response
-  getUser(token, function(response){
+  getUser(token).then(function (response){
+    console.log({response})
     const {apiKey} = response
-    getPictures(apiKey, function(response){
-      const {pic} = response
-      console.log(pic)
-    })
-  })
-  // getPictures(apiKey, function(response){
-  //   const {pictures} = response
-  //   console.log(pictures)
-  // })
+    console.log(apiKey)
+  }).catch(err => console.log(err.message))
 })
-
-
-
-// const user = login("Muhammad Baddar");
-// console.log(user.token)
-
-// const apiKey = getUser(user.token);
-// console.log(apiKey)
-
-// const getUserPictures = getPictures(apiKey);
-// console.log(getUserPictures)
